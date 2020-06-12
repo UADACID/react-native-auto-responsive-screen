108fbf44da11bae38d6e1307230412708fc5481c
tama <pratama.sa@meteor.id>
2020-06-12 19:31:52 +0700

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+import { Dimensions, Platform, PixelRatio } from 'react-native'
+const { width, height } = Dimensions.get('window')
+const SCREEN_WIDTH = width;
+const SCREEN_HEIGHT = height;
+
+class ResponsiveScreen {
+
+    static init(width, height) {
+        this.wscale = SCREEN_WIDTH / width;
+        this.hscale = SCREEN_HEIGHT / height;
+    }
+
+    static normalize = (size, based) => {
+        const newSize = based === 'height' ? size * this.hscale : size * this.wscale;
+        if (Platform.OS === 'ios') {
+            return Math.round(PixelRatio.roundToNearestPixel(newSize));
+        } else {
+            return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+        }
+    }
+}
+
+export default ResponsiveScreen
